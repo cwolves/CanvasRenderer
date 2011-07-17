ba153e9c7acc58e61fa4aaed7a1248115f459c32
Mark Kahn <cwolves@gmail.com>
2011-07-17 00:53:00 -0700

    Adding per-level font rendering in cases where there is letter-spacing set

--- source/text-renderer.js
+++ source/text-renderer.js
@@ -17,20 +17,38 @@ html2canvas.prototype.analyzeFont = function( txt, rect ){
 		.setFont        ( style + ' ' + variant + ' ' + weight + ' ' + size + ' ' + family )
 //		.setTextAlign   ( align )   // TODO: CSS has "justify", Canvas doesn't
 		.setTextBaseline( 'bottom' ); // TODO: these don't match up between CSS & Canvas, adjust later
+
+	switch(transform){
+		case 'capitalize':
+			txt.nodeValue = txt.nodeValue.replace(/(\W)(\w)/g, function(m, space, letter){
+				return space + letter.toUpperCase();
+			});
+			break;
+		case 'uppercase':
+			txt.nodeValue = txt.nodeValue.toUpperCase();
+			break;
+		case 'lowercase':
+			txt.nodeValue = txt.nodeValue.toLowerCase();
+			break;
+	}
 }
 
 html2canvas.prototype.drawText = function( txt, rect ){
 	var range = document.createRange();
 	range.selectNode( txt );
 
-	var splt = range.toString().split(/\s/),
-	     len = splt.length,
-	  chrCtr = 0,
-	      ix = 0,
+	// Try to draw each word, but if letter spacing is set, we can't draw per word since
+	//   canvas letter-spacing can't be set, so we need to draw each letter.
+	var   node = txt.parentNode,
+	    lSpace = parseFloat( this.$.letterSpacing( node ) ) || 0,
+	      splt = range.toString().split(lSpace ? "" : /\b/),
+	       len = splt.length,
+	    chrCtr = 0,
+	        ix = 0,
 	    strLen;
 
 	while(ix < len){
-		while( (ix < len) && !splt[ ix ] ){ ix++; chrCtr++; } // skip past white-space
+		while( (ix < len) && (!splt[ ix ] || (splt[ix] == ' ' )) ){ ix++; chrCtr++; } // skip past white-space
 		if(ix >= len){ break; }
 
 		strLen = splt[ ix ].length;
@@ -44,7 +62,7 @@ html2canvas.prototype.drawText = function( txt, rect ){
 			this.ctx.fillText( splt[ ix ], rect.left, rect.top + rect.height);
 		}
 
-		chrCtr += strLen + 1;
+		chrCtr += strLen;// + 1;
 		ix++;
 	}
 }
