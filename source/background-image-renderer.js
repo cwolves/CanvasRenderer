9fe2cb5d0bc1c0bbccef9d00331a4c81dca366cb
Mark Kahn <cwolves@gmail.com>
2011-07-17 17:39:21 -0700

    Adding support for background-size

--- source/background-image-renderer.js
+++ source/background-image-renderer.js
@@ -1,18 +1,25 @@
 html2canvas.prototype.renderBackgroundImage = function( node, rect, box, imgPath ){
-	var bgImg = this.getImage( imgPath ),
-	        x = 0,
-	        y = 0;
+	var bgImg = this.getImage( imgPath );
 
 	if( !bgImg ){ return; } // image couldn't be loaded for some reason
 
 	var bgRepeat = this.$.backgroundRepeat  ( node ),
 	  bgPosition = this.$.backgroundPosition( node ).split(' '),
+	      bgSize = this.$.backgroundSize    ( node ),
+	  bgSizeSplt = bgSize.split(' '),
 	     repeatX = (bgRepeat != 'no-repeat') && (bgRepeat != 'repeat-y'),
 	     repeatY = (bgRepeat != 'no-repeat') && (bgRepeat != 'repeat-x'),
                w = bgImg.width,
                h = bgImg.height;
 	 bgPositionX = this.$.unitsToPx( bgPosition[0], rect.width , repeatX ? rect.width  : w ),
-	 bgPositionY = this.$.unitsToPx( bgPosition[1], rect.height, repeatY ? rect.height : h );
+	 bgPositionY = this.$.unitsToPx( bgPosition[1], rect.height, repeatY ? rect.height : h ),
+	     bgSizeX = this.$.unitsToPx( bgSizeSplt[0], rect.width  ),
+	     bgSizeY = this.$.unitsToPx( bgSizeSplt[1], rect.height ),
+	           x = 0,
+	           y = 0;
+
+	if(bgSizeX){ w = bgSizeX; }
+	if(bgSizeY){ h = bgSizeY; }
 
 	while( x < rect.width ){
 		while( y < rect.height ){
