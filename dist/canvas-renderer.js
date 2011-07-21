ee21c83fa8bd4bb3c48dc8ee8d62234c82ce99b1
Mark Kahn <cwolves@gmail.com>
2011-07-21 15:12:38 -0700

    Adding distributions

--- dist/canvas-renderer.js
+++ dist/canvas-renderer.js
@@ -3,7 +3,7 @@
  *
  * Copyright 2011, Mark Kahn
  *
- * Date: Sun Jul 17 17:40:20 2011 -0700
+ * Date: Mon Jul 18 12:13:56 2011 -0700
  */
 function html2canvas( opts, loadCB ){
 	this.$       = html2canvas.bridge;
@@ -640,42 +640,3 @@ CanvasRenderingContext2D.prototype.dashedLineTo = function (fromX, fromY, toX, t
 		dash = !dash;
 	}
 };
-(function( $ ){
-	var canvas, visible, info, loaded;
-
-	function toggleText(){
-		canvas[ visible ? 'hide' : 'show' ]();
-		info.innerText = visible ? 'HTML' : 'Canvas';
-		visible ^= 1;
-	}		
-
-	$.addEvent( document.body, 'click', function(){
-		if( !canvas ){
-			var start = +new Date();
-			canvas = new html2canvas(
-				{ css : 'position: absolute; top: 0px; left: 0px; z-index: 10000' },
-				function(){
-					var start2 = +new Date();
-
-					window.scrollTop = document.body.scrollTop = 0;
-
-					canvas
-						.render      ( document.body )
-						.appendToNode( document.body );
-
-					info               = $.createElement( 'DIV' );
-					info.style.cssText = 'position : fixed; top: 0px; right: 0px; z-index: 10000; padding : 5px; border: 2px solid #EEE; border-width: 0 0 2px 2px;';
-					document.body.appendChild( info );
-
-					toggleText();
-					loaded = true;
-
-					var end = +new Date();
-					this.console.log('Loading Time: ' + (start2-start) + '; Render Time: ' + (end - start2));
-				}
-			);
-		}
-
-		loaded && toggleText();
-	});
-})( html2canvas.bridge );
