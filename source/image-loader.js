46289f2ab0dc5f183cb182e480df5e5baf05e757
Mark Kahn <cwolves@gmail.com>
2011-07-21 19:29:49 -0700

    Adding same-origin check

--- source/image-loader.js
+++ source/image-loader.js
@@ -1,11 +1,20 @@
 var imgs = {},
      cbs = [];
 
+html2canvas.prototype.sameOrigin = function( src ){
+	var a = this.$.createElement('a');
+	a.href = src;
+
+	return (a.host === location.host) && (a.protocol === location.protocol);
+};
+
 html2canvas.prototype.getImage = function( path ){
 	return imgs[ path ] && imgs[ path ].img;
 };
 
 html2canvas.prototype.addImage = function( path, cb ){
+	if( !this.sameOrigin( path ) ){ return; }
+
 	if( imgs[ path ] ){
 		if( imgs[ path ].loaded ){
 			cb();
@@ -19,11 +28,11 @@ html2canvas.prototype.addImage = function( path, cb ){
 	var img = new Image(),
 	    cbs = [ cb ],
 
-	 imgObj = {
+	imgObj = {
 		img    : img,
 		cbs    : cbs,
 		loaded : false
-	 };
+	};
 
 	img.onload = function(){
 		this.loaded = true;
@@ -62,6 +71,7 @@ html2canvas.prototype.init.push(function( opts ){
 	for(var i=0, l=allNodes.length; i<l; i++){
 		var bgImage = this.$.backgroundImage( allNodes[i] );
 		if(!bgImage || ( bgImage == 'none' )){ continue; }
+		console.log(bgImage);
 
 		ctr++;
 		this.addImage( bgImage, done );
