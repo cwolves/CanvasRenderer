3f91c356ba82118b0361d49a1fb45cc6e8990c6c
Mark Kahn <cwolves@gmail.com>
2011-07-21 19:29:37 -0700

    Adding baseHref property from loader

--- source/loader.js
+++ source/loader.js
@@ -1,11 +1,30 @@
 (function(){
-	var base = 'http://canvasrenderer.clientsite.me/dist/canvas-renderer-ui-',
+	var base = 'http://canvasrenderer.clientsite.me/',
+	    path = 'dist/canvas-renderer-ui-',
 	     scr = document.createElement('SCRIPT');
 
-	scr.src = window.jQuery
-		? base + 'min.js'
-		: base + 'jquery-min.js';
+	scr.src = base + path +
+		(window.jQuery
+			? 'min.js'
+			: 'jquery-min.js'
+		);
 
 	scr.type = 'text/javascript';
+
+	scr.onload = function(){
+		if(window.html2canvas){
+			html2canvas.baseHref = base;
+			scr.onload = function(){};
+		}else{
+			setTimeout( scr.onload, 100 );
+		}
+	};
+
+	scr.onreadystatechange = function () {
+		if ( ~' complete loaded '.indexOf( this.readyState ) ){
+			scr.onload();
+		}
+	}
+
 	document.getElementsByTagName('HEAD')[0].appendChild(scr);
 })();
